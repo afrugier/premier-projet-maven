952a0796341efde106836af1c5c3eeccf9b2632b
Anthony Frugier <fru.anthony@gmail.com>
2017-07-03 16:02:59 +0200

    Ajout d'un uber jar dans premier-projet-maven

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -6,12 +6,10 @@ import com.github.lalyos.jfiglet.FigletFont;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        String asciiArt = FigletFont.convertOneLine("Hello");
-        System.out.println(asciiArt);
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		String asciiArt = FigletFont.convertOneLine("Hello");
+		System.out.println(asciiArt);
+	}
 }
