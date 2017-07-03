ee7ae234fef1dc21d66f158a14d9bdaead998f54
Anthony Frugier <fru.anthony@gmail.com>
2017-07-03 16:05:44 +0200

    Modification du texte

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -9,7 +9,7 @@ import com.github.lalyos.jfiglet.FigletFont;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		String asciiArt = FigletFont.convertOneLine("Hello");
+		String asciiArt = FigletFont.convertOneLine("Hello World !!");
 		System.out.println(asciiArt);
 	}
 }
